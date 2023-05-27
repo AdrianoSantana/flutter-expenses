26fe12a05f788104f8285b2b75b7d34522a90eea
AdrianoSantana <45883008+AdrianoSantana@users.noreply.github.com>
2023-05-27 08:46:43 -0300

    feat: style container

--- lib/main.dart
+++ lib/main.dart
@@ -25,24 +25,50 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Despesas pessoais'),
-        ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Card(
-              elevation: 5,
-              child: Text('Gráfico de despesas'),
-            ),
-            Column(
-                children: _transactions
-                    .map((tr) => Card(
-                          child: Text(tr.title),
-                        ))
-                    .toList()),
-          ],
-        ));
+      appBar: AppBar(
+        title: const Text('Despesas pessoais'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Card(
+            elevation: 5,
+            child: Text('Gráfico de despesas'),
+          ),
+          Column(
+              children: _transactions
+                  .map((tr) => Card(
+                        child: Row(
+                          children: [
+                            Container(
+                              margin: const EdgeInsets.symmetric(
+                                horizontal: 15.0,
+                                vertical: 10.0,
+                              ),
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                  color: Colors.black,
+                                  width: 2,
+                                ),
+                              ),
+                              padding: const EdgeInsets.all(12.0),
+                              child: Text(
+                                tr.value.toString(),
+                              ),
+                            ),
+                            Column(
+                              children: [
+                                Text(tr.title),
+                                Text(tr.date.toLocal().toString())
+                              ],
+                            ),
+                          ],
+                        ),
+                      ))
+                  .toList()),
+        ],
+      ),
+    );
   }
 }
